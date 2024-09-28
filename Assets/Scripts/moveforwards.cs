9fc471eb5f19cb4e23b3f7abf2ba92c0ace3420a
tmur647 <tmur647@aucklanduni.ac.nz>
2024-09-29 10:05:24 +1300

    feat: add smoke to previous ambulance

--- Assets/Scripts/moveforwards.cs
+++ Assets/Scripts/moveforwards.cs
@@ -4,13 +4,10 @@ using UnityEngine;
 
 public class moveforwards : MonoBehaviour
 {
-    // Speed of the car (modifiable in the Inspector)
     public float speed = 10f;
 
-    // Update is called once per frame
     void Update()
     {
-        // Move the car forward along the world's Z-axis (ground plane)
         transform.Translate(-Vector3.forward * speed * Time.deltaTime, Space.World);
     }
 }
